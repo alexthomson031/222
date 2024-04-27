b62914a0c61432f064e31143a574af65ba4b344b
Netotest <athomson@mail.ru>
2024-04-27 21:16:36 +0300

    Initiak commit

---
+++ src/BmiService.java
@@ -0,0 +1,10 @@
+public class BmiService {
+
+    public int index(int weight, int high) {
+        int result;
+        float truehigh = (float) high * high / 10000;
+        result = (int) (weight / truehigh);
+        return result;
+
+    }
+}
